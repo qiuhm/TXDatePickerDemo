2dd8f748ff9838ae8840ddc2bc431014b6982712
萱的软件测试博客 <qiuhmm@gmail.com>
2013-11-30 17:39:21 +0800

    Initial Commit

---
+++ TXDatePickerDemo/txAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  txAppDelegate.h
+//  TXDatePickerDemo
+//
+//  Created by tingxuan.qhm on 13-11-30.
+//  Copyright (c) 2013年 tingxuan.qhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface txAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
